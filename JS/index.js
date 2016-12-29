94c1b90742bdc2688b1aa55828348acc5e2669d1
davidE <delias3791@gmail.com>
2016-12-29 13:16:11 +0200

    Completed generating the layout, notes are included for what to do next.

--- JS/index.js
+++ JS/index.js
@@ -3,10 +3,29 @@
  */
 var COLUMNS = 3;
 var rows = 4;
-
+var CLICK = false;
 function init(){
-    createMain();
-    createCards();
+    var welcomeText = document.createElement("h1");
+    welcomeText.id = "welcomeStuff";
+    welcomeText.innerHTML = "Welcome to the match making game!";
+    document.body.appendChild(welcomeText);
+    var startButton= document.createElement("input");
+    startButton.type = "submit";
+    startButton.id = "startButton";
+    startButton.addEventListener("click", playGame);
+    startButton.value = "Let's play a game!";
+    document.body.appendChild(startButton);
+}
+function playGame(){
+    if (CLICK === false){
+        createMain();
+        createCards();
+        var removeClick = document.getElementById("startButton");
+        removeClick.removeEventListener("click", playGame);
+    }
+    else {
+        CLICK = true;
+    }
 }
 function createMain(){
     var mainCont = document.createElement("div");
@@ -21,12 +40,37 @@ function createCards() {
         rowDivs.classList = "rows";
         mainCont.appendChild(rowDivs);
         for (var j =0; j < COLUMNS; j++){
+            // var flipDiv = document.createElement("div"); these relate to flip animation
+            // flipDiv.className = "card effect__click";
             var cardDivs = document.createElement("div");
             cardDivs.className += "cardDivs col-xs-4";
-            
             cardDivs.id = "column"+i+"-row"+j;
+            // cardDivs.addEventListener("click", revealCard);
             rowDivs.appendChild(cardDivs);
         }
     }
-
+    getCardClass();
 }
+function getCardClass(){
+    var getTheClass = document.querySelectorAll(".cardDivs");
+    for (var i =0; i < getTheClass.length; i++){
+        console.log("This is the "+i+" card in the array.");
+        // getTheClass[i].style  = "content: url("+"../Images/"+i+".jpg"+");"
+    }
+}
+//Create array of images, randomize.
+//Randomize unique setAttribute.Data.image when creating each card that points to an image from the array.
+//On click, change ID
+// Flag clicks for t/f clicks with card
+//If true, remove event listener
+//cards unflipped
+// function cardTimeOut(){
+//     setTimeout()
+// }
+// function revealCard(){
+//     setInterval(flipCard, 2000);
+// }
+// function flipCard() {
+//     // document.getElem
+// }
+init();
